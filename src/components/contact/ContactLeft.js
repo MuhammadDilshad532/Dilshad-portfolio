921029ea33ab957e8de4077179ff7e6bdf911f99
MuhammadDilshad532 <dilshadahamad532280@gmail.com>
2024-07-31 12:04:25 +0500

    contect form done

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import React from "react";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
 import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
@@ -11,37 +16,49 @@ const ContactLeft = () => {
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">Muhammad Dilshad</h3>
         <p className="text-lg font-normal text-gray-400">
-          MERN Stack Developer
+          React.js/Next.js Developer
         </p>
         <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+          I am a React.js developer skilled in HTML, CSS, Tailwind CSS,
+          Bootstrap, JavaScript, and React.js, focusing on user-friendly
+          interfaces and responsive web apps.
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
+          Phone: <span className="text-lightText">+92 3266125280</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email: <span className="text-lightText">mdtechnical47@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
-            <FaFacebookF />
-          </span>
-          <span className="bannerIcon">
-            <FaTwitter />
-          </span>
-          <span className="bannerIcon">
-            <FaLinkedinIn />
-          </span>
+          <a href="https://www.facebook.com/profile.php?id=100080294441134">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+          </a>
+          <a href="https://github.com/MuhammadDilshad532">
+            <span className="bannerIcon">
+              <FaGithub />
+            </span>
+          </a>
+          <a href="https://www.linkedin.com/in/muhammaddilshad542/">
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </a>
+          <a href="https://www.instagram.com/muhammaddilshad7672/">
+            <span className="bannerIcon">
+              <FaInstagram />
+            </span>
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ContactLeft
+export default ContactLeft;
