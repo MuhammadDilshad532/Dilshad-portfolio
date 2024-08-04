4b2ad74c6cb4fab3eaa3a058086313bbe86e0afa
MuhammadDilshad532 <dilshadahamad532280@gmail.com>
2024-08-04 18:08:52 +0500

    Experience update

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import ResumeCard from "./ResumeCard";
 
 const Experience = () => {
@@ -11,52 +11,52 @@ const Experience = () => {
     >
       <div>
         <div className="flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2010 - 2022</p>
+          <p className="text-sm text-designColor tracking-[4px]">2023 - 2024</p>
           <h2 className="text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            title="React.js Developer"
+            subTitle="CodeSprint- (2023 - Present)"
+            result="Pakistan"
+            des="CodeSprint hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+            title="Next.js Developer"
+            subTitle="Securehops Developer Team - (2023 - 2024)"
+            result="Pakistan"
+            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Pakistan isn't easy."
           />
           <ResumeCard
             title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+            subTitle="CodeSprint - (2023 - 2024)"
+            result="Pakistan"
+            des="The Pakistan economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
           />
         </div>
       </div>
       <div>
         <div className="flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2001 - 2020</p>
+          <p className="text-sm text-designColor tracking-[4px]">2020 - 2023</p>
           <h2 className="text-4xl font-bold">Trainer Experience</h2>
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Gym Instructor"
-            subTitle="Rainbow Gym Center (2015 - 2020)"
-            result="DHAKA"
+            subTitle="Rainbow Gym Center (2020 - 2021)"
+            result="Pakistan"
             des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
           />
           <ResumeCard
             title="Web Developer and Instructor"
-            subTitle="SuperKing College (2010 - 2014)"
-            result="CANADA"
+            subTitle="SuperKing College (2021 - 2022)"
+            result="Pakistan"
             des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
           />
           <ResumeCard
             title="School Teacher"
-            subTitle="Kingstar Secondary School (2001 - 2010)"
-            result="NEVADA"
+            subTitle="Kingstar Secondary School (2022 - 2023)"
+            result="Pakistan"
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
           />
         </div>
