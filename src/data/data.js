20a943398d4fad719eb2ef56f31c5325b6889313
Dilshad <dilshadahamad532280@gmail.com>
2024-08-27 15:01:33 +0500

    update the Features icons

--- src/data/data.js
+++ src/data/data.js
@@ -1,41 +1,42 @@
 import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe } from "react-icons/fa";
-import { SiProgress, SiAntdesign } from "react-icons/si";
+import { FaMobile, FaReact, FaJsSquare, FaGithub, FaGlobe } from "react-icons/fa";
+import { SiProgress, SiNextdotjs, SiTailwindcss, SiAntdesign, SiCss3 } from "react-icons/si";
 
 // Features Data
 export const featuresData = [
   {
     id: 1,
+    icon: <FaReact />,
     title: "React.js Developer",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
   {
     id: 2,
-    icon: <AiFillAppstore />,
+    icon: <SiNextdotjs />,
     title: "Next.js Rendering",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
   {
     id: 3,
-    icon: <SiProgress />,
-    title: "Responsive Design",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
-  {
-    id: 4,
-    icon: <FaMobile />,
-    title: "HTML & CSS",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
-  {
-    id: 5,
-    icon: <SiAntdesign />,
+    icon: <FaJsSquare />,
     title: "JavaScript Coding",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
   {
+    id: 4,
+    icon: <SiTailwindcss />,
+    title: "Tailwind Css",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 5,
+    icon: <SiCss3 />,
+    title: "HTML & CSS",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
     id: 6,
-    icon: <FaGlobe />,
+    icon: <FaGithub />,
     title: "Git & GitHub",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
