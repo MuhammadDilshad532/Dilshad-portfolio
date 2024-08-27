6ae10fd6bc43b808ee56151b0e9dcdd20900c739
Dilshad <dilshadahamad532280@gmail.com>
2024-08-27 16:24:36 +0500

    update the Features text and Features done

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -17,7 +17,7 @@ const LeftBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h4 className=" text-lg font-normal">WELCOME TO MY PORTFOLIO</h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">M.Dilshad</span>
         </h1>
@@ -30,9 +30,7 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+        I am a React.js developer skilled in HTML, CSS, Tailwind CSS, Bootstrap, JavaScript, React.js and Next.js , focusing on user-friendly interfaces and responsive web apps.
         </p>
       </div>
       {/* Media */}
