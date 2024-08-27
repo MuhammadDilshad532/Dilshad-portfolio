aa29ca4801b657c29258af0b8f71c7873bd44407
Dilshad <dilshadahamad532280@gmail.com>
2024-08-27 18:26:13 +0500

    update the text of My Projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -18,38 +18,32 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title=" Codesprint-web"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Codesprint-web is an interactive platform for coding challenges, built with React.js and Tailwind CSS for dynamic performance."
           src={projectOne}
         />
         <ProjectsCard
           title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Developed an e-commerce website with React.js, offering seamless shopping experiences, secure transactions, and responsive design."
           src={projectTwo}
         />
         <ProjectsCard
           title="Rubngub.com"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Rubngub.com is an all-in-one platform offering organic products, featuring a user-friendly interface and secure transactions."
           src={projectThree}
         />
         <ProjectsCard
           title="Personal-portfolio"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" My personal portfolio showcases my skills and projects, featuring a clean design and detailed project descriptions"
           src={projectFour}
         />
         <ProjectsCard
           title="Access.pk"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Access.pk is a comprehensive platform offering easy access to services, products, and user-friendly navigation."
           src={projectfive}
         />
         <ProjectsCard
           title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" A feature-rich chatting app enabling real-time messaging, media sharing, and secure conversations with a user-friendly interface."
           src={projectSex}
         />
       </div>
